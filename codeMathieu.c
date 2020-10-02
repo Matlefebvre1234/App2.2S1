765df516588d719c1d93bd46c5b3cbef10df6a57
Mathieu <mat_lefebvre12@hotmail.com>
2020-10-02 14:23:42 -0400

    fini

--- codeMathieu.c
+++ codeMathieu.c
@@ -1,72 +1,64 @@
 
 #include <stdio.h>
-#define  COLONNE 3
+#define  COLONNE 2
 #define  RANGEE 3
 #define  MATRICE_CARREE 3
 
 int DetecterPalindrome(char valeur[]);
 void AdditionMatrices(int[RANGEE][COLONNE],int[RANGEE][COLONNE],int[RANGEE][COLONNE]);
 void MultiplicationMatrices(int[MATRICE_CARREE][MATRICE_CARREE],int[MATRICE_CARREE][MATRICE_CARREE],int[MATRICE_CARREE][MATRICE_CARREE]);
-void AfficherMatrice(int[RANGEE][COLONNE]);
+void AfficherMatriceAdd(int[RANGEE][COLONNE]);
+void AfficherMatriceMul(int[MATRICE_CARREE][MATRICE_CARREE]);
 void PlanDeTest();
+void TestPalindrome(char texte[],char resultatAttendu[]);
+void TestAdditionMatrice(int[RANGEE][COLONNE],int[RANGEE][COLONNE],int[RANGEE][COLONNE],int[RANGEE][COLONNE]);
+void TestMultiplicationMatrice(int[MATRICE_CARREE][MATRICE_CARREE],int[MATRICE_CARREE][MATRICE_CARREE],int[MATRICE_CARREE][MATRICE_CARREE],int[MATRICE_CARREE][MATRICE_CARREE]);
 
 
-
+int main(int argc, char **argv)
+{
+	
+    PlanDeTest();	
+	return 0;
+}
 
 void PlanDeTest()
 {
-	//Valide la fonction DetecterPalindrome
-	//Test1 laval
-	char valeur[] = {"laval"};
-	int confirmation = 0;
+	//Validation palindrome
+	//test1 laval
+	TestPalindrome("laval","vrai");
 	
-	printf("------------------------VALIDATION PALINDROME----------------------------\n");
-	confirmation = DetecterPalindrome(valeur);
-	printf("%s",valeur);
-	printf(" : ");
-	if(confirmation == 1)
-	{
-		printf("vraie");
-	}
-	else
-	{
-			printf("Faux");	
-	}
-	printf("\n");
+	//test2 LAVAL
+	TestPalindrome("LAVAL","vrai");
 	
-	//Test 2 LAVAL
-	confirmation = DetecterPalindrome("LAVAL");
-	printf("%s","LAVAL");
-	printf(" : ");
-	if(confirmation == 1)
-	{
-		printf("vraie");
-	}
-	else
-	{
-			printf("Faux");	
-	}
-	printf("\n");
+	//test3 LAval
+	TestPalindrome("LAval","Faux");
 	
-	//Test 3 LAval
-	confirmation = DetecterPalindrome("LAval");
-	printf("%s","LAval");
-	printf(" : ");
-	if(confirmation == 1)
-	{
-		printf("vraie");
-	}
-	else
-	{
-			printf("Faux");	
-	}
-	printf("\n");
-	printf("\n");
 	
-	//Validation de la fonction AdditionMatrices
-	printf("------------------------VALIDATION Addition de matrice----------------------------\n");
 	
-	//Test 1 
+	
+	//Validation addition de matrice
+	
+	int matrice1[RANGEE][COLONNE] =   {	{1,2},
+										{3,4},
+										{5,6}};
+										
+	int matrice2[RANGEE][COLONNE] =   {	{6,5},
+										{4,3},
+										{2,1}};							
+	
+	int matriceResultante[RANGEE][COLONNE] =  {	{0,0},
+												{0,0},
+												{0,0}};
+											
+	int matriceAttendu[RANGEE][COLONNE] =  {	{7,7},
+												{7,7},
+												{7,7}};
+	
+	TestAdditionMatrice(matrice1,matrice2,matriceResultante,matriceAttendu);
+	
+	/*
+	//Test 1 additon
 	int matriceAdd1[RANGEE][COLONNE]={	{1,2,3},
 										{4,5,6},
 										{7,8,9}};	
@@ -77,18 +69,16 @@ void PlanDeTest()
 										
 	int matriceAdd3[RANGEE][COLONNE]={	{0,0,0},
 										{0,0,0},
-										{0,0,0}};	
+										{0,0,0}};
 										
-	AdditionMatrices(matriceAdd1,matriceAdd2,matriceAdd3);
+	int matriceResAttendu[RANGEE][COLONNE]= {{2,4,6}, 
+											{8,10,12},
+											{14,16,18}};		
+										
+										
+	TestAdditionMatrice(matriceAdd1,matriceAdd2,matriceAdd3,matriceResAttendu);
 	
-	AfficherMatrice(matriceAdd1);
-	printf(" +\n");
-	AfficherMatrice(matriceAdd2);
-	printf(" =\n");
-	AfficherMatrice(matriceAdd3);
-	printf("\n\n\n\n\n");
-	
-	//Test2
+	//Test2 addition avec chiffre negatif
 	
 	int matriceAdd4[RANGEE][COLONNE]={	{-1,-2,-3},
 										{-4,-5,-6},
@@ -102,57 +92,106 @@ void PlanDeTest()
 										{1,1,1},
 										{1,1,1}};	
 										
-	AdditionMatrices(matriceAdd4,matriceAdd5,matriceAdd6);
-	
-	AfficherMatrice(matriceAdd1);
-	printf(" +\n");
-	AfficherMatrice(matriceAdd2);
-	printf(" =\n");
-	AfficherMatrice(matriceAdd3);
-	printf("\n\n\n\n\n");
-	
-	//Test3 2x2
-	/*int matriceAdd7[2][2]={	{2,3},
-							{5,-6}};
-											
-
-	int matriceAdd8[2][2]={	{5,2},
-							{-4,7}};
-										
-	int matriceAdd9[2][2]={	{0,0},
-							{0,0}};	
-										
-	AdditionMatrices(matriceAdd7,matriceAdd8,matriceAdd9);
-	
-	AfficherMatrice(matriceAdd7);
-	printf(" +\n");
-	AfficherMatrice(matriceAdd8);
-	printf(" =\n");
-	AfficherMatrice(matriceAdd9);
-	printf("\n\n\n\n\n");*/
-	
-	//Test4
-	
-	//MultiplicationMatrices(matriceAdd1,matriceAdd2,matriceAdd3);
-	//AfficherMatrice(matriceAdd3);										
-		
+	int matriceResAttendu2[RANGEE][COLONNE]= {{0,0,0}, 
+											{0,0,0},
+											{0,0,0}};	
+	TestAdditionMatrice(matriceAdd4,matriceAdd5,matriceAdd6,matriceResAttendu2);
 	
 	
 	
-	
-	
-	
+	//Validation multiplication de matrices
+	//Test 1 matriceAdd1 x matriceAdd2
+	int matriceResAttendu4[RANGEE][COLONNE]= {{30,36,42}, 
+											{66,81,96},
+											{102,126,150}};											
+	TestMultiplicationMatrice(matriceAdd1,matriceAdd2,matriceAdd3,matriceResAttendu4);
+	//test 2 multiplication par 0
+	TestMultiplicationMatrice(matriceAdd1,matriceResAttendu2,matriceAdd3,matriceResAttendu2);
+	*/
 }
 
-
-
-
-int main(int argc, char **argv)
+void TestMultiplicationMatrice(int m1[MATRICE_CARREE][MATRICE_CARREE],int m2[MATRICE_CARREE][MATRICE_CARREE],int mf[MATRICE_CARREE][MATRICE_CARREE],int mRa[MATRICE_CARREE][MATRICE_CARREE])
 {
+	//m1 : matrice 1
+	//m2 :matrice 2
+	//mf : matrice résultante de l'Addition de m1 et m2
+	//mRa : matrice du résulat attendu
 	
-    PlanDeTest();	
-	return 0;
+	
+		//Validation de la fonction AdditionMatrices
+	printf("------------------------VALIDATION Multiplication de matrices----------------------------\n");
+	
+	//Test 1 
+										
+	MultiplicationMatrices(m1,m2,mf);
+	AfficherMatriceMul(m1);
+	printf(" x\n");
+	AfficherMatriceMul(m2);
+	printf(" =\n");
+	AfficherMatriceMul(mf);
+	printf("\nResultat attendu :\n");
+	AfficherMatriceMul(mRa);
+	printf("\n\n\n");
 }
+
+
+void TestAdditionMatrice(int m1[RANGEE][COLONNE],int m2[RANGEE][COLONNE],int mf[RANGEE][COLONNE],int mRa[RANGEE][COLONNE])
+{
+	//m1 : matrice 1
+	//m2 :matrice 2
+	//mf : matrice résultante de l'Addition de m1 et m2
+	//mRa : matrice du résulat attendu
+	
+	
+	//Validation de la fonction AdditionMatrices
+	printf("------------------------VALIDATION Addition de matrices----------------------------\n");
+	
+	//Test 1 
+										
+	AdditionMatrices(m1,m2,mf);
+	AfficherMatriceAdd(m1);
+	printf(" +\n");
+	AfficherMatriceAdd(m2);
+	printf(" =\n");
+	AfficherMatriceAdd(mf);
+	printf("\nResultat attendu :\n");
+	AfficherMatriceAdd(mRa);
+	printf("\n\n\n");
+
+	
+	}
+	
+void TestPalindrome(char texte[],char resultatAttendu[])
+{
+	//Valide la fonction DetecterPalindrome
+	//Test1 laval
+	
+	int confirmation = 0;
+	
+	confirmation = DetecterPalindrome(texte);
+	printf("------------------------VALIDATION PALINDROME----------------------------\n");
+	printf("Parametre entre : ");
+	printf("%s",texte);
+	printf("\nResultat: ");
+	if(confirmation == 1)
+	{
+		printf("vrai");
+	}
+	else
+	{
+			printf("Faux");	
+	}
+	printf("\nResulat Attendu: ");
+	printf("%s",resultatAttendu);
+	printf("\n");
+
+}
+
+
+
+
+
+
 
 int DetecterPalindrome(char valeur[]) 
 {
@@ -203,9 +242,6 @@ void AdditionMatrices(int matrice1[RANGEE][COLONNE],int matrice2[RANGEE][COLONNE
 		
 	}
 	
-	
-	
-	
 }
 
 
@@ -232,19 +268,12 @@ void MultiplicationMatrices(int matrice1[MATRICE_CARREE][MATRICE_CARREE],int mat
 
 			}
 		
-	}
-	
-	
-	
-	
-	
-	
-	
+	}	
 }
 
 
 
-void AfficherMatrice(int matrice[RANGEE][COLONNE])
+void AfficherMatriceAdd(int matrice[RANGEE][COLONNE])
 {
 	
 	for(int i =0;i<RANGEE ;i++)
@@ -262,6 +291,28 @@ void AfficherMatrice(int matrice[RANGEE][COLONNE])
 		 
 	}
 }
+
+void AfficherMatriceMul(int matrice[MATRICE_CARREE][MATRICE_CARREE])
+{
+	
+	for(int i =0;i<RANGEE ;i++)
+	{
+			printf("| ");
+			for(int j =0; j < COLONNE; j++)
+			
+			{
+			
+				printf("%d",matrice[i][j]);
+				printf(" ");
+
+			}
+		 printf("|\n");
+		 
+	}
+}
+
+
+
 
 
 
